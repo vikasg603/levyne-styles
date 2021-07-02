9d2ad36478cb93573f4272a015ce1df7427a5754
Vikas <vikas.rg@somaiya.edu>
2021-07-03 00:04:41 +0530

    sass

--- sass.js
+++ sass.js
@@ -1,12 +1,12 @@
 const sass = require('node-sass');
 const fs = require('fs');
 sass.render({
-  file: '/Users/vikas/Documents/LevyneComplete/levyne-styles/flone/scss/style.scss'
+  file: './flone/scss/style.scss'
 }, function(err, result) {
     if(err) {
       console.log(err);
       process.exit();
     }
-    fs.writeFileSync('./style.css', result.css);
+    fs.writeFileSync('./flone/css/style.css', result.css);
     process.exit();
 });
