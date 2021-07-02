d9ae8019da4582220ee831e251ae7392faa93339
Vikas <vikas.rg@somaiya.edu>
2021-07-03 00:34:12 +0530

    checking import once

--- sass.js
+++ sass.js
@@ -1,7 +1,14 @@
 const sass = require('node-sass');
 const fs = require('fs');
+const importOnce = require('node-sass-import-once');
 sass.render({
-  file: './flone/scss/style.scss'
+  file: './flone/scss/style.scss',
+  importer: importOnce,
+  importOnce: {
+    index: false,
+    css: false,
+    bower: false
+  }
 }, function(err, result) {
     if(err) {
       console.log(err);
